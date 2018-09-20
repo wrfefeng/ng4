7dde6bc8d1dcae471163dec296b68f8f99ed15d3
wangruifeng <1371934898@qq.com>
2018-09-20 16:59:37 +0800

    2018-09-19 商品组件和星级评价组件

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-start',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start.component.css']
 })
 export class StartComponent implements OnInit {
-
+  private stars: boolean[];
+  @Input()
+  private rating:number  = 0;
   constructor() { }
 
   ngOnInit() {
+    this.stars = []
+    for (let i = 1; i <= 5; i ++) {
+      this.stars.push(i > this.rating);
+    }
   }
 
 }
